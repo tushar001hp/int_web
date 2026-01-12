3e901a3b3dad1825defddfc52c132f5e3591f244
tushar001hp <tusharjamwal001@gmail.com>
2026-01-12 12:10:20 +0530

    Initial commit

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-
 dotenv.config();
 const app = express();
 
